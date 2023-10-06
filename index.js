56be16d7e8fd43b85cfb26de13370bca5f576ece
Thembakazi <thembakazingamlana25@gmail.com>
2023-10-06 17:07:17 +0200

    ädding change

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import exphbs from 'express-handlebars';
 import bodyParser from 'body-parser';
-import waiterAvailabilityApp from './waiter.js';
 import session from 'express-session';
 
 const app = express();
@@ -10,7 +9,7 @@ app.use(session({ secret: 'your-secret-key', resave: false, saveUninitialized: t
 const handlebars = exphbs.create({
     extname: '.handlebars',
     defaultLayout: false,
-    layoutDir:'./views/layouts',
+    layoutDir: './views/layouts',
 });
 
 app.engine('handlebars', handlebars.engine);
@@ -21,18 +20,18 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Redirect from root URL to /home
+// Redirect from root URL to /waiter
 app.get('/', (req, res) => {
-    res.redirect('/home');
+    res.redirect('/waiter');
 });
 
 // Route to serve the login form
-app.get('/home', (req, res) => {
-    res.render('login');
+app.get('/waiter', (req, res) => {
+    res.render('waiter');
 });
 
 // Handle form submission
-app.post('/login', (req, res) => {
+app.post('/waiter', (req, res) => {
     // Handle login logic here, including validation and authentication
     // Example: You can access form data using req.body.role, req.body.username, and req.body.password
     // Check credentials and perform login actions
@@ -41,14 +40,66 @@ app.post('/login', (req, res) => {
     res.redirect('/dashboard');
 });
 
+// Define a route for waiter schedules
+app.get('/waiter/Thembi', (req, res) => {
+    // Render the Thembi's page
+    res.render('thembi', {
+      title: 'Welcome, Thembi!',
+      schedule: [
+        'Monday: Morning shift',
+        'Wednesday: Evening shift',
+        'Thursday: Evening shift',
+        'Friday: Day off',
+        'Tuesday: Day off',
+      ],
+    });
+  });
+  app.get('/waiter/Angel', (req, res) => {
+    // Render the Thembi's page
+    res.render('Angel', {
+      title: 'Welcome, Angel!',
+      schedule: [
+        'Tuesday: Morning shift',
+        'Saturday: Evening shift',
+        'Friday: Evening shift',
+        'Sunday: Day off',
+        'Monday: Day off',
+        'Wensday: Day off',
+        'Thursday: Day off',
+      ],
+    });
+  });
+  app.get('/waiter/Sipho', (req, res) => {
+    // Render the Thembi's page
+    res.render('Sipho', {
+      title: 'Welcome, Sipho!',
+      schedule: [
+        'Friday: Morning shift',
+        'Wednesday: Evening shift',
+        'Monday: Evening shift',
+        'Tuesady: Day off',
+        'Saturday: Day off',
+        'Sunday: Day off',
+        'Thursday: Day off',
+      ],
+    });
+  });
+
+app.post('/waiter/update/:waiterName', (req, res) => {
+    const waiterName = req.params.waiterName;
+    const selectedDays = req.body.days; // An array of selected days
+
+    // Update the waiter's schedule with the selected days
+    // Perform any necessary logic to update the schedule
+
+    // Redirect back to the waiter's schedule page
+    res.redirect(`/waiter/${waiterName}`);
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
-
-
-
-
 
 
 
